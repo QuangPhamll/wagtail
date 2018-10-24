f9523bb7096125817c348b55eeb286dcf990c2f3
quangphamll <quang.pham.fr@gmail.com>
2018-10-24 12:57:13 +0200

    add Tag and page to list all tag for site

--- wagtail_site/blog/models.py
+++ wagtail_site/blog/models.py
@@ -70,3 +70,16 @@ class BlogPageGalleryImage(Orderable):
         FieldPanel('caption'),
     ]
 
+
+class BlogTagIndexPage(Page):
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+
+        return context
